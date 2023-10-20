eb5f0a483b1a4a646b248a3acfa72072c7b2031b
Matt <148349808+IXRMatt@users.noreply.github.com>
2023-10-20 11:20:09 +0100

    Create main.py
    added code

---
+++ main.py
@@ -0,0 +1,43 @@
+# This code is based on the following example:
+# https://discordpy.readthedocs.io/en/stable/quickstart.html#a-minimal-bot
+
+import os
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+#This is it logging into the bot
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+#This is the bot's prefix, I have made a simple chat function so if you type /hello
+#It will respond
+    if message.content.startswith('/hello'):
+        await message.channel.send('Hello!')
+
+#This is logging into the token
+try:
+  token = os.getenv("TOKEN") or ""
+  if token == "":
+    raise Exception("Please add your token to the Secrets pane.")
+  client.run(token)
+except discord.HTTPException as x:
+    if x.status == 429:
+        print(
+            "The Discord servers denied the connection for making too many requests"
+        )
+        print(
+            "Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests"
+        )
+    else:
+        raise x
